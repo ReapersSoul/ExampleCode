a58e0a4ed41c9f8a8299615c4a63cb854b2c7787
Trevor Ducharme <56406337+ReapersSoul@users.noreply.github.com>
2023-06-14 22:03:01 -0400

    removed test function in palendrome

--- js/IsPalendrome.js
+++ js/IsPalendrome.js
@@ -9,23 +9,4 @@ function IsPalendrome(str) {
     //return true if rev and tmpstring are the same
     return rev == tmpstr;
 }
-function TestPalendrome(PrintTests) {
-    //save results for return value
-    var Test1Result = IsPalendrome("A man a plan a canal panama");
-    var Test2Result = IsPalendrome("racecar");
-    var Test3Result = IsPalendrome("");
-    var Test4Result = IsPalendrome("A large rat");
-    var Test5Result = IsPalendrome("Random words dont make a good sentance");
-    //print test cases if PrintTests is true
-    if (PrintTests) {
-        console.log("Test Case # : Result : Expected Result");
-        console.log("Test Case 1 : ".concat(Test1Result, " : true"));
-        console.log("Test Case 2 : ".concat(Test2Result, " : true"));
-        console.log("Test Case 3 : ".concat(Test3Result, " : true"));
-        console.log("Test Case 4 : ".concat(Test4Result, " : false"));
-        console.log("Test Case 5 : ".concat(Test5Result, " : false"));
-    }
-    //return true if all test cases matched their expected results
-    return Test1Result && Test2Result && Test3Result && !(Test4Result && Test5Result);
-}
 module.exports = IsPalendrome;
--- src/IsPalendrome.ts
+++ src/IsPalendrome.ts
@@ -9,24 +9,4 @@ function IsPalendrome(str:string) {
     return rev == tmpstr;
 }
 
-function TestPalendrome(PrintTests:boolean) {
-    //save results for return value
-    var Test1Result=IsPalendrome("A man a plan a canal panama");
-    var Test2Result=IsPalendrome("racecar");
-    var Test3Result=IsPalendrome("");
-    var Test4Result=IsPalendrome("A large rat");
-    var Test5Result=IsPalendrome("Random words dont make a good sentance");
-    //print test cases if PrintTests is true
-    if(PrintTests){
-        console.log("Test Case # : Result : Expected Result");
-        console.log(`Test Case 1 : ${Test1Result} : true`);
-        console.log(`Test Case 2 : ${Test2Result} : true`);
-        console.log(`Test Case 3 : ${Test3Result} : true`);
-        console.log(`Test Case 4 : ${Test4Result} : false`);
-        console.log(`Test Case 5 : ${Test5Result} : false`);
-    }
-    //return true if all test cases matched their expected results
-    return Test1Result&&Test2Result&&Test3Result&&!(Test4Result&&Test5Result);
-}
-
 module.exports = IsPalendrome;
